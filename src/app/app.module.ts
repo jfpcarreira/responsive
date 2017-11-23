556b2628560e856e1418188a4997a25303ba917c
Joao Carreira <jfpcarreira@gmail.com>
2017-11-22 22:02:31 -0500

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { TesteComponent } from './components/teste/teste.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TesteComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
