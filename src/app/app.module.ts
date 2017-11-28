989fa60dcca7dc20dcff9c42f6ef05f8b231c70c
Joao Carreira <jfpcarreira@gmail.com>
2017-11-27 23:07:57 -0500

    User info a funcionar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TesteComponent } from './components/teste/teste.component';
+import { TopbarComponent } from './components/layout/topbar/topbar.component';
+import { LeftmenuComponent } from './components/layout/leftmenu/leftmenu.component';
+import { MaincontentComponent } from './components/layout/maincontent/maincontent.component';
+import { BreadcrumbComponent } from './components/layout/breadcrumb/breadcrumb.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TesteComponent
+    TesteComponent,
+    TopbarComponent,
+    LeftmenuComponent,
+    MaincontentComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule
